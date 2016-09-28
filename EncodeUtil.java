a6ef15a42808495a401bcbc1ec3cc891b7458781
zhanglianxin <zhanglianxin1993@163.com>
2016-09-28 22:15:55 +0800

    add EncodeUtil.java

---
+++ EncodeUtil.java
@@ -0,0 +1,30 @@
+﻿package test;
+
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class EncodeUtil {
+
+    public static char[] encode(String string) throws Exception {
+        char[] chs = string.toCharArray();
+        String result = "";
+        for (char c : chs) {
+            result = result + "\\" + Integer.toHexString(c)/* + " " */;
+        }
+
+        Encoder encoder = Base64.getEncoder();
+
+        byte[] bytes = encoder.encode(result.getBytes());
+        char[] cs = new char[bytes.length];
+
+        for (int i = 0; i < bytes.length; i++) {
+//            System.out.print(bytes[i] + " ");
+            cs[i] = (char) bytes[i];
+        }
+
+//        System.out.println(cs);
+        
+        return cs;
+    }
+    
+}
